1c2b3d9b1f0c6ad2cf6596efe70e97c14472b852
Huy Hieu PHAM <hieuhuy01@gmail.com>
2018-09-24 14:59:17 +0200

    Update DenseNet-MSR-Action3D-AS1.py

--- DenseNet-MSR-Action3D-AS1.py
+++ DenseNet-MSR-Action3D-AS1.py
@@ -35,8 +35,8 @@ nb_classes = 8
 
 # Learning rate schedule.
 def step_decay(epoch):
-	initial_lrate = 0.001
-	drop = 0.1
+	initial_lrate = 3e-4
+	drop = 0.5
 	epochs_drop = 100.0
 	lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
 	return lrate
@@ -48,7 +48,7 @@ validation_data_dir = 'data/MSR-Action3D/AS1/validation'
 nb_train_samples = 3762
 nb_validation_samples = 2205
 epochs = 250
-batch_size = 128
+batch_size = 64
 
 if K.image_data_format() == 'channels_first':
     input_shape = (3, img_width, img_height)
